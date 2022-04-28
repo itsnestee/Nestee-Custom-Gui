cc90e5d10a2abec77444976ea47fc591bef07624
Robert Nesta Nuhu <nesteemusicprovvidence@gmail.com>
2022-04-28 14:06:03 +0200

    Nestee Default Colours

---
+++ GUI Components/Colours.js
@@ -0,0 +1,33 @@
+Content.makeFrontInterface(600, 600);
+
+include("TextButton.js");
+
+const var Panel1 = Content.getComponent("Panel1");
+
+Panel1.setPaintRoutine(function(g)
+{
+	g.fillAll(nBlack);
+
+});
+
+const var Button1 = Content.getComponent("Button1");function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
